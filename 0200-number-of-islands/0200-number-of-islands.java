edfda020746618731026e422481f3234643f1682
avnee888 <avneegaur2004@gmail.com>
2026-08-07 22:18:08 +0530

    Time: 3 ms (87.5%), Space: 52.4 MB (33.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,33 @@
 class Solution {
     char[][] grid;
     int row,col;
-  
-    int[][] dir={{0,1},{0,-1},{1,0},{-1,0}};
+    int[][] dir={{0,1},{1,0},{-1,0},{0,-1}};
     public void dfs(int i,int j){
         if(i<0||j<0||j>=col||i>=row||grid[i][j]!='1'){
-           
-           
-           return;
+          return;
         }
-        
-        
         grid[i][j]='x';
-        for(int[] d:dir){
+        for(int[]d:dir){
             int newrow=i+d[0];
             int newcol=j+d[1];
             dfs(newrow,newcol);
         }
-        return ;
+        return;
     }
     public int numIslands(char[][] grid) {
         this.grid=grid;
         row=grid.length;
         col=grid[0].length;
-        int ans=0;
+        int count=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]=='1'){
-              dfs(i,j);
-              ans++ ; 
+                   dfs(i,j);
+                   count++;
                 }
             }
         }
-        return ans;
+
+        return count;
     }
 }
